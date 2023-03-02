8a6ce383a159ca20f4b07d552adf16ca950b2710
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2023-03-02 02:07:18 -0800

    is it a leaf?

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - check if a node is a leaf
+ * @node: the node to check
+ * Return: true if it's a node or false otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node != NULL && (node->left != NULL || node->right != NULL))
+		return (1);
+	return (0);
+}
