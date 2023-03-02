365707728f54b7a624524a22def2c8d36c2ec1c9
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2023-03-02 07:05:47 -0800

    pre-order traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - traverses the tre in pre-order
+ * @tree: the root 
+ * @func: function to print a single node value
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && func != NULL)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
